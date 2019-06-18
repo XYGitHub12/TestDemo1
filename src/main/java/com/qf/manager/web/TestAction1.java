6b9839a3793baa92b2980741c73199a109974cfa
xueyu <1375156056@qq.com>
2019-06-18 13:31:07 +0800

    第一次提交（主架）

---
+++ src/main/java/com/qf/manager/web/TestAction1.java
@@ -0,0 +1,13 @@
+package com.qf.manager.web;
+
+/**
+ * @Author XY
+ * @Date 2019/6/18 13:27
+ * @Version 1.0
+ **/
+public class TestAction1 {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
