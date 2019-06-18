79caacf869d5541ee8bda8a5871c65689885713d
xueyu <1375156056@qq.com>
2019-06-18 13:40:26 +0800

    第二次提交（增加了打印语句）

--- src/main/java/com/qf/manager/web/TestAction1.java
+++ src/main/java/com/qf/manager/web/TestAction1.java
@@ -1,13 +1,9 @@
 package com.qf.manager.web;
 
-/**
- * @Author XY
- * @Date 2019/6/18 13:27
- * @Version 1.0
- **/
 public class TestAction1 {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("薛宇，您好！");
     }
 }
